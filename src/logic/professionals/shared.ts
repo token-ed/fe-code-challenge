7904ce4e8bdf9a461ff3fb8cdca80d2a1eff3ebf
Ed <emiguel.dev@gmail.com>
2024-07-20 21:15:08 +0200

    Fixed endpoint

--- src/logic/professionals/shared.ts
+++ src/logic/professionals/shared.ts
@@ -1,5 +1,4 @@
-export const CATEGORIES_ENDPOINT =
-    `http://localhost:4001/categories`;
+export const CATEGORIES_ENDPOINT = `https://my-json-server.typicode.com/token-ed/fe-code-challenge/categories`;
 
 export const PROFESSIONALS_ENDPOINT =
-    "http://localhost:4001/professionals";
+  "https://my-json-server.typicode.com/token-ed/fe-code-challenge/professionals";
